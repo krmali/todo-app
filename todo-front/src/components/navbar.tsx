9f84a34d4b03238b1ecec7674260a8fd27b5c767
Kareem Ali <kareem.ali.rahman@gmail.com>
2022-08-20 20:17:18 +0200

    small fix to todos ui

--- todo-front/src/components/navbar.tsx
+++ todo-front/src/components/navbar.tsx
@@ -1,86 +1,78 @@
 import { ReactNode, useContext } from 'react';
 import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
+    Box,
+    Flex,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    useDisclosure,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
-import Todos from './todos';
 import { AuthContext } from '../providers/auth_provider';
 
-const Links = ['Dashboard', 'Projects', 'Team'];
 
 const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
-  </Link>
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+    </Link>
 );
 
 export default function Navbar() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { user, logout } = useContext(AuthContext);
 
-  return (
-    <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box fontWeight="bold" color="brown">TODO-APP</Box>
-          <Link
-            px={2}
-            py={1}
-            rounded={'md'}
-            _hover={{
-              textDecoration: 'none',
-              bg: useColorModeValue('gray.200', 'gray.700'),
-            }}
-            href={'https://github.com/krmali/todo-app'}>
-            github
-          </Link>
-          </HStack>
-          <Flex alignItems={'center'}>
-            <Button
-              variant={'solid'}
-              colorScheme={'orange'}
-              size={'sm'}
-              mr={4}
-              leftIcon={<CloseIcon/>}
-              onClick={() => logout()} 
-              >
-                logout 
-            </Button>
-          </Flex>
-        </Flex>
+    return (
+        <>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <IconButton
+                        size={'md'}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={'Open Menu'}
+                        display={{ md: 'none' }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <HStack spacing={8} alignItems={'center'}>
+                        <Box fontWeight="bold" color="brown">TODO-APP</Box>
+                        <Box fontWeight="bold" color="gray.700">hello {user!.username}</Box>
+                        <Link
+                            px={2}
+                            py={1}
+                            rounded={'md'}
+                            _hover={{
+                                textDecoration: 'none',
+                                bg: useColorModeValue('blue.200', 'blue.700'),
+                            }}
+                            href={'https://github.com/krmali/todo-app'}>
+                            github
+                        </Link>
+                    </HStack>
+                    <Flex alignItems={'center'}>
+                        <Button
+                            variant={'solid'}
+                            colorScheme={'orange'}
+                            size={'sm'}
+                            mr={4}
+                            leftIcon={<CloseIcon />}
+                            onClick={() => logout()}
+                        >
+                            logout
+                        </Button>
+                    </Flex>
+                </Flex>
 
-      </Box>
+            </Box>
 
-    </>
-  );
+        </>
+    );
 }
--- todo-front/src/components/todos.tsx
+++ todo-front/src/components/todos.tsx
@@ -1,17 +1,33 @@
+import { Center, Spinner, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
 import {useQuery} from 'react-query';
 import {getTodos} from '../api/api'
 import Todo from './todo';
 
 const Todos = () => {
     const query = useQuery('todos',  getTodos);
+    const [isErrorShown, setIsErrorShown] = useState<boolean>(false);
+    const toast = useToast();
 
     if (query.isLoading || query.isIdle) {
-        return <h1>Loading...</h1>;
+        // return <h1>Loading...</h1>;
+        return   <Center><Spinner size='xl' /></Center>;
+
     }
     if (query.isError) {
-        // const error = query.data;
-        return <span>Error</span>
+        if(!isErrorShown){
+            toast({
+              title: 'Network Error',
+              description: "oops, it seems that there is a technical issue.",
+              status: 'error',
+              duration: 1000,
+              isClosable: true,
+            })
+            setIsErrorShown(true);
+        }
+        return <></>;
     }
+
     return(
         <ul>
             {query.data.map(td => 
@@ -25,4 +41,8 @@ const Todos = () => {
 }
 
 export default Todos;
+
+function toast(arg0: { title: string; description: string; status: string; duration: number; isClosable: boolean; }) {
+    throw new Error('Function not implemented.');
+}
 
